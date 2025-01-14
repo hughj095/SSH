74500440860e9246e340eda797f4dc8474c3a8c2
John <john.m.hughes84@outlook.com>
2025-01-13 20:01:44 -0500

    jh

---
+++ config.py
@@ -0,0 +1,4 @@
+SMTP_SERVER = 'smtp.gmail.com'
+SENDER = 'hughesjm314@gmail.com'
+SMTP_PORT = 587
+
--- ssh_health_check.py
+++ ssh_health_check.py
@@ -1,3 +1,68 @@
 ### Remote laptop health check with SSH
 
 import paramiko
+import smtplib
+from email.mime.text import MIMEText
+import config 
+
+#variables
+SERVER = {
+    "hostname": "192.168.1.2",
+    "port" : 22,
+    "username" : "john.m.hughes84@outlook.com",
+    "password" : "6347"
+}
+RECIPIENT = 'john.m.hughes84@outlook.com'
+
+
+#functions
+def connect_to_server(server):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(server["hostname"],  
+            server["port"], 
+            server["username"], 
+            server["password"])  
+        print("Connected to server")
+        return client
+    except Exception as e:
+        print(f'Error connecting to server: {e}')
+        return None
+
+def execute_command(client, command):
+    stdin, stdout, stderr = client.exec_command(command)
+    return stdout.read().decode()
+
+def check_health(client):
+    cpu_command = "top -bn1 | grep Cpu"
+    disk_command = "df -h /"
+    cpu_usage = float(execute_command(client, cpu_command).split()[1])
+    disk_usage = int(execute_command(client, disk_command).split()[4][:-1])
+    print(f'CPU Usage is {cpu_usage}%')
+    print(f'Disk Usage is {disk_usage}%')
+    alerts = []
+    if cpu_usage > 80:
+        alerts.append(f'CPU usage is {cpu_usage}%')
+    if disk_usage > 80:
+        alerts.append(f'Disk usage is {disk_usage}%')
+    return alerts
+
+def send_email(alerts):
+    sender = config.SENDER
+    recipient = RECIPIENT
+    subject = "Server Health Check"
+    body = "\n".join(alerts)
+
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipient
+
+    try:
+        with smtplib.SMTP_SSL(config.SMTP_SERVER, config.SMTP_PORT) as server:
+            server.login(config.SMTP_USER, config.SMTP_PASSWORD)
+            server.sendmail(sender, recipient, msg.as_string())
+            print("Email sent successfully")
+    except Exception as e:
+        print(f'Error sending email: {e}')
